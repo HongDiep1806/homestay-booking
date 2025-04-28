f9ef70ee8fe9d361d4af7fa24d4588f234500cc0
HongDiep1806 <diep.tuthi.cit21@eiu.edu.vn>
2025-04-28 18:26:31 +0700

    update database

--- HomestayBooking/Models/AppUser.cs
+++ HomestayBooking/Models/AppUser.cs
@@ -2,21 +2,16 @@
 
 namespace HomestayBooking.Models
 {
-    public class Staff
+    public class AppUser: IdentityUser
     {
-        public int StaffID { get; set; }
         public string FullName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
         public string IdentityCard { get; set; }
         public string Gender { get; set; }
         public DateOnly DOB { get; set; }
-        public string HashPassword { get; set; }
         public string Address { get; set; }
-        public bool IsActive { get; set; }
+        public bool IsStaff { get; set; }
+        public bool? IsActive { get; set; }
         public ICollection<Booking> Bookings { get; set; }
-        public int RoleID { get; set; }
-        public Role Role { get; set; }
-        
+
     }
 }
--- HomestayBooking/obj/Debug/net8.0/HomestayBooking.AssemblyInfo.cs
+++ HomestayBooking/obj/Debug/net8.0/HomestayBooking.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomestayBooking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25732fd35bbaff7f22fa44c5ddde5fd2a6c62ca7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c70fb0a9e2a2579beb3262d2139e228dd146c465")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomestayBooking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomestayBooking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
